efde6977b3780a4463bf84b5494f6c9fea962230
Marius <marius.sergent@gmail.com>
2025-03-11 11:05:17 +0100

    add goback on product add

--- src/screens/AddProductScreen.jsx
+++ src/screens/AddProductScreen.jsx
@@ -2,9 +2,10 @@ import { StyleSheet, View } from 'react-native'
 import AddProductForm from '../components/form/AddProductForm'
 import { addProduct } from '../services/wc-api'
 
-function AddProductScreen () {
+function AddProductScreen ({ navigation }) {
   const handleSubmit = async (product) => {
     const result = await addProduct(product)
+    navigation.goBack()
   }
   return (
     <View style={styles.container}>
