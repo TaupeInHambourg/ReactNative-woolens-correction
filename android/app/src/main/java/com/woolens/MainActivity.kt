e23181f329b8d69f69c65ad3c08bb3895c14f1cf
Marius <marius.sergent@gmail.com>
2025-03-11 14:09:07 +0100

    add splashscreen

--- android/app/src/main/java/com/woolens/MainActivity.kt
+++ android/app/src/main/java/com/woolens/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.woolens
 
-import android.os.Bundle;
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -9,10 +10,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
+   override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(null) // super.onCreate(null) with react-native-screens
   }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
